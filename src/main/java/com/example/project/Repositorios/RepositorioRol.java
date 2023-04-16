a7bfcebae1939f32b71777ee4f5642aedae7832f
Marlon Almanza <marlondavidag44@outlook.com>
2023-04-16 15:50:31 -0500

    Eliminando CRUD de role V2

--- src/main/java/com/example/project/Repositorios/RepositorioRol.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.Repositorios;
-
-import com.example.project.Modelos.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositorioRol extends JpaRepository<Rol, String> {}
